218b738ae29ba39a0fd12250c3d8fe953475e41e
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-02-17 23:02:48 +0100

    Added play and queue commands

---
+++ commands/play.js
@@ -0,0 +1,103 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { QueryType } = require("discord-player");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("play")
+    .setDescription("Puszcza muzykę z Youtuba!")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("szukaj")
+        .setDescription("Szuka utworu po frazie lub linku")
+        .addStringOption((option) =>
+          option
+            .setName("fraza")
+            .setDescription("Wyszukiwana fraza lub link do utworu")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("playlista")
+        .setDescription("Szuka playlisty po linku")
+        .addStringOption((option) =>
+          option
+            .setName("url")
+            .setDescription("URL do playlisty")
+            .setRequired(true)
+        )
+    ),
+  run: async ({ client, interaction }) => {
+    const voiceChannel = interaction.member.voice.channel;
+
+    if (!voiceChannel)
+      return interaction
+        .editReply(":x: Musisz być na kanale głosowym!")
+        .then((msg) => {
+          setTimeout(() => msg.delete(), 3000);
+        });
+
+    if (
+      !voiceChannel.permissionsFor(client.user).has("ViewChannel") ||
+      !voiceChannel.permissionsFor(client.user).has("Connect") ||
+      !voiceChannel.permissionsFor(client.user).has("Speak")
+    )
+      return interaction
+        .editReply(":x: Nie mogę dołączyć do kanału głosowego!")
+        .then((msg) => {
+          setTimeout(() => msg.delete(), 3000);
+        });
+
+    const queue = await client.player.createQueue(interaction.guild);
+    if (!queue.connection) await queue.connect(voiceChannel);
+
+    let embed = new EmbedBuilder();
+
+    if (interaction.options.getSubcommand() === "playlista") {
+      let url = interaction.options.getString("url");
+      const result = await client.player.search(url, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.YOUTUBE_PLAYLIST,
+      });
+      if (result.tracks.length === 0)
+        return interaction
+          .editReply(":x: Nie znaleziono utworu")
+          .then((msg) => {
+            setTimeout(() => msg.delete(), 3000);
+          });
+
+      const playlist = result.playlist;
+      const song = result.tracks[0];
+      await queue.addTracks(result.tracks);
+      embed
+        .setTitle(`Dodano **${result.tracks.length}** utworów do kolejki`)
+        .setDescription(`[**${playlist.title}**] (${playlist.url})`)
+        .setThumbnail(song.thumbnail)
+        .setFooter({ text: `Dodano przez ${song.requestedBy.tag}` });
+    } else if (interaction.options.getSubcommand() === "szukaj") {
+      let url = interaction.options.getString("fraza");
+      const result = await client.player.search(url, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.AUTO,
+      });
+      if (result.tracks.length === 0)
+        return interaction
+          .editReply(":x: Nie znaleziono utworu")
+          .then((msg) => {
+            setTimeout(() => msg.delete(), 3000);
+          });
+
+      const song = result.tracks[0];
+      await queue.addTrack(song);
+      embed
+        .setTitle("Dodano utwór do kolejki")
+        .setDescription(`[**${song.title}**](${song.url}) [${song.duration}]`)
+        .setThumbnail(song.thumbnail)
+        .setFooter({ text: `Dodano przez ${song.requestedBy.tag}` });
+    }
+    if (!queue.playing) await queue.play();
+    await interaction.editReply({ embeds: [embed] }).then((msg) => {
+      setTimeout(() => msg.delete(), 10000);
+    });
+  },
+};
---
+++ index.js
@@ -0,0 +1,92 @@
+const { Client, GatewayIntentBits, Collection, ActivityType } = require("discord.js");
+const dotenv = require("dotenv");
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
+const { Player } = require("discord-player");
+
+dotenv.config();
+const TOKEN = process.env.TOKEN;
+
+const LOAD_SLASH = process.argv[2] == "load";
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.slashcommands = new Collection();
+client.player = new Player(client, {
+  ytdlOptions: {
+    quality: "highestaudio",
+    highWaterMark: 1 << 25,
+  },
+});
+
+let commands = [];
+const slashFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of slashFiles) {
+  const slashcmd = require(`./commands/${file}`);
+  if ("data" in slashcmd && "run" in slashcmd) {
+    client.slashcommands.set(slashcmd.data.name, slashcmd);
+  } else {
+    console.log(
+      `[WARNING] The command at ./commands/${file} is missing a required "data" or "run" property.`
+    );
+  }
+  if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
+}
+
+if (LOAD_SLASH) {
+  const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+  (async () => {
+    try {
+      console.log(
+        `Started refreshing ${commands.length} application (/) commands.`
+      );
+      const data = await rest.put(
+        Routes.applicationCommands(CLIENT_ID, GUILD_ID),
+        { body: commands }
+      );
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands.`
+      );
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  })();
+} else {
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+      activities: [
+        { name: `/play`, type: ActivityType.Listening },
+      ],
+      status: "online",
+    });
+  });
+
+  client.on("interactionCreate", (interaction) => {
+    async function handleCommand() {
+      if (!interaction.isCommand()) return;
+
+      const slashcmd = client.slashcommands.get(interaction.commandName);
+      if (!slashcmd) interaction.reply("Command not found");
+
+      await interaction.deferReply();
+      await slashcmd.run({ client, interaction });
+    }
+    handleCommand();
+  });
+  client.login(TOKEN);
+}
