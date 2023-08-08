27ab88e5900124339a33836f01977ec696f6fdf2
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-08-08 17:38:40 +0200

    Added logging of playerStart

--- events/playerStart.js
+++ events/playerStart.js
@@ -1,10 +1,14 @@
 const { GuildQueueEvent } = require("discord-player");
-const { printNowPlaying } = require("../functions");
+const { printNowPlaying, logInfoDate } = require("../functions");
 
 module.exports = {
   name: GuildQueueEvent.playerStart,
   type: "player",
   async execute(queue) {
+    logInfoDate(
+      `Track ${queue.currentTrack.title} (${queue.currentTrack.url}) started playing at ${queue.guild.name}`,
+      2
+    );
     printNowPlaying(queue.metadata, queue);
   },
 };
